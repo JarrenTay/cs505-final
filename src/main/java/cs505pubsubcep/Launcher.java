974dde36f20ef2e2c1fc56bc53f6c5bb52b8030f
Jarren Tay <jarrentayusa@gmail.com>
2020-04-27 18:50:47 -0400

    Adding hospital nodes to db

--- src/main/java/cs505pubsubcep/Launcher.java
+++ src/main/java/cs505pubsubcep/Launcher.java
@@ -34,6 +34,8 @@ public class Launcher {
     public static String inputStreamName = null;
     public static long accessCount = -1;
     public static String zipDetailsFilePath = "/home/jta255/finalProject/cs505-pubsub-cep-template/data/kyzipdetails.csv";
+    public static String hospitalsFilePath = "/home/jta255/finalProject/cs505-pubsub-cep-template/data/hospitals.csv";
+    public static String distanceFilePath = "/home/jta255/finalProject/cs505-pubsub-cep-template/data/kyzipdistance.csv";
     public static Map<String, Integer> zipCodeCases;
     public static List<String> zipList;
     public static int positiveCases = 0;
@@ -45,6 +47,8 @@ public class Launcher {
     public static CEPEngine statusEngine = null;
 
     private static Driver driver;
+    //private static DatabaseManagementService managementService;
+    //public static GraphDatabaseService graphDb;
 
     public static void main(String[] args) throws IOException {
 
@@ -96,6 +100,7 @@ public class Launcher {
         statusEngine.createCEP(inputStreamName, outputStreamName, inputStreamAttributesString, outputStreamAttributesString, rtr2);
 
         driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "cs505pass"));
+/*
         try ( Session session = driver.session() )
         {
             String greeting = session.writeTransaction( new TransactionWork<String>()
@@ -112,7 +117,58 @@ public class Launcher {
             } );
             System.out.println( greeting );
         }
+*/
+/*
+        try (Session session = driver.session() ) {
+            session.writeTransaction( tx -> {
+                tx.run ("MATCH (n) DETACH DELETE n");
+                return 1;
+            } );
+        }
 
+        addHospitalNodes(hospitalsFilePath, driver);
+        addHospitalRelationships(distanceFilePath, driver);
+*/
+        try (Session session = driver.session()) {
+            session.readTransaction( tx -> {
+                Result result = tx.run ("MATCH (a:Hospital) RETURN a.zip ORDER BY a.zip");
+                while (result.hasNext() ) {
+                    System.out.println( Integer.toString(result.next().get(0).asInt()));
+                }
+                return 1;
+            });
+        }
+
+/*
+        managementService = new DatabaseManagementServiceBuilder("/home/jta255/finalProject/hospitalDb");
+        graphDb = managementService.database("HospitalDb");
+        registerShutdownHook(managementService);
+//        var connectionType = RelationshipType.withName("CONNECTS");
+        var startNode;
+        var endNode;
+        var newNode;
+        var relationship;
+        var newRelationship;
+        try (var transaction = database.beginTx()) {
+            startNode = transaction.createNode("Hospital");
+            startNode.setProperty("hospitalId", 11740202);
+            startNode.setProperty("beds", 404);
+            startNode.setProperty("zip", 40202);
+            endNode = transaction.createNode("Hospital");
+            endNode.setProperty("hospitalId", 11640536);
+            endNode.setProperty("beds", 604);
+            endNode.setProperty("zip", 40536);
+            relationship = startNode.createRelationshipTo(endNode, RelTypes.CONNECTED);
+            relationship.setProperty("distance", 55.46403169);
+            transaction.commit();
+        }
+        try (var transaction = database.beginTx()) {
+            newNode = transaction.findNode("Hospital", "hospitalId", 11740202);
+            transaction.commit();
+        }
+        System.out.println("TEST");
+        System.out.println(Integer.toString(newNode.getProperty("beds")));
+*/
 
         System.out.println("CEP Started...");
 
@@ -168,8 +224,62 @@ public class Launcher {
         //System.out.println(outMap.toString());
         return outMap;
     }
-}
 
+    private static void addHospitalNodes(String hospitalsFile, Driver driver) {
+        Path pathToFile = Paths.get(hospitalsFile);
+        try (BufferedReader br = Files.newBufferedReader(pathToFile, StandardCharsets.UTF_8)) {
+            String line = br.readLine();
+            line = br.readLine();
+            int count = 0;
+            while (line != null) {
+                String[] attributes = line.split(",");
+                System.out.println(attributes[0]);
+                try (Session session = driver.session() ) { 
+                    session.writeTransaction( tx -> {
+                        tx.run ("CREATE (a:Hospital {hospitalId: $hospitalId, zip: $zip, beds: $beds, level: $level})", parameters("hospitalId", new Integer(attributes[0]), "beds", new Integer(attributes[7]), "zip", new Integer(attributes[5]), "level", attributes[16]));
+                        return 1;
+                    } );
+                }
+                System.out.println(Integer.toString(count));
+                count = count + 1;
+                line = br.readLine();
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    private static void addHospitalRelationships(String distanceFile, Driver driver) {
+        Path pathToFile = Paths.get(distanceFile);
+        try (BufferedReader br = Files.newBufferedReader(pathToFile, StandardCharsets.US_ASCII)) {
+            String line = br.readLine();
+            line = br.readLine();
+            int count = 0;
+            while (line != null) {
+                String[] attributes = line.split(",");
+                try (Session session = driver.session() ) {
+                    session.writeTransaction( tx -> {
+                        tx.run ("MATCH (a:Hospital),(b:Hospital) WHERE a.zip = $aZip AND b.zip = $bZip CREATE(a)-[r:CONNECTED_TO {distance: $distance}]->(b)", parameters("aZip", attributes[0], "bZip", attributes[1], "distance", attributes[2]));
+                        return 1;
+                    } );
+                }
+                try (Session session = driver.session() ) {
+                    session.writeTransaction( tx -> {
+                        tx.run ("MATCH (a:Hospital),(b:Hospital) WHERE a.zip = $aZip AND b.zip = $bZip CREATE(a)-[r:CONNECTED_TO {distance: $distance}]->(b)", parameters("aZip", new Integer(attributes[1]), "bZip", new Integer(attributes[0]), "distance", new Float(attributes[2])));
+                        return 1;
+                    } );
+                }
+                if (count % 100 == 0) {
+                    System.out.println(Integer.toString(count));
+                }
+                count = count + 1;
+                line = br.readLine();
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+}
 /*
 public class CEPQuery {
     public static List<Map<String, Map<String, String>>> zipList;
